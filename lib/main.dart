b3617d556d52233ed92a06ee07c64362a973af19
0vermore <63291966+0vermore@users.noreply.github.com>
2020-11-24 22:51:30 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Main page'),
+      initialRoute: '/',
+      routes: {
+        // When navigating to the "" route, build the FirstScreen widget.
+        '': (context) => MyApp(),
+        // When navigating to the "second" route, build the SecondScreen widget.
+        'second': (context) => NextPage(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+       return Scaffold(
+      appBar: AppBar(
+                title: Text(widget.title),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Next'),
+          onPressed: () => {
+            Navigator.pushNamed(context, 'second')// navigate to second route
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class NextPage extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Back'),
+          onPressed: () => {
+            Navigator.pop(context)// navigate to first route
+          },
+        ),
+      ),
+    );
+  }
+}
